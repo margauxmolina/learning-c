b3cb5fb81f57b393ef1e3622b1053eb8d38bea51
Margaux Molina <margauxmolina@Margauxs-MacBook-Pro.local>
2026-03-13 16:01:26 -0400

    changing method to display percent correctly

--- script.c
+++ script.c
@@ -5,13 +5,19 @@ int main() {
     printf("Hello world!\n");
     printf("testing\n");
 
-    //testing variables 
+    //testing variables for profit calulation
     const double costPrice = 4.55; 
     double sellingPrice = 8.99;
+    double secondsuppliercost = 3.50;
     sellingPrice = 12.66;
     double take = sellingPrice - costPrice;
     printf("The profit is: %f\n", take);
-    int profitMargin = (take / costPrice) * 100;
+
+
+    double profitMargin = (take / costPrice) * 100;
+    
+    // Convert to integer for whole number percentage
+    profitMargin = (int)profitMargin; 
     printf("The profit margin is: %d%%\n", profitMargin);
 
 
