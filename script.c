5a589f72dcaddbeb85fe5ad7fb50a499b959fb8f
Margaux Molina <margauxmolina@Margauxs-MacBook-Pro.local>
2026-03-13 11:54:33 -0400

    ading in variables and testing with hypothetical par numbers

--- script.c
+++ script.c
@@ -4,4 +4,11 @@ int main() {
     //first c code 
     printf("Hello world!\n");
     printf("testing\n");
+
+    //testing variables 
+    double costPrice = 4.55; 
+    double sellingPrice = 8.99;
+    double take = sellingPrice - costPrice;
+    printf("The profit is: %f\n", take);
+
 }
